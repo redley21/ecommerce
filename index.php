28ff17ba7ef0c881a9dc69ff5389a44cc10b5f85
redley21 <redley_redward@hotmail.com>
2019-10-07 15:32:44 -0400

    autoload hcodebr classes

--- index.php
+++ index.php
@@ -7,8 +7,11 @@ $app = new \Slim\Slim();
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
-	echo "OK";
+	
+	$sql = new Hcode\DB\Sql();
+	$results = $sql->select("SELECT * FROM tb_users");
+
+	echo json_encode($results);
 
 });
 
